ef7618650256090d6dde8eacfd2e812c25798112
Vincent Leighton <vincentleighton@gmail.com>
2021-10-30 15:56:36 -0700

    Different layouts added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(
+    /A simple app to switch beween several layouts. The layouts will be designed using CSS/i
+  );
+  expect(linkElement).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Header</header>
-      <body>Body</body>
+      <header className="App-header">
+        A simple app to switch beween several layouts. The layouts will be
+        designed using CSS
+      </header>
+      <body className="App-body">
+        {/* Hotdog layout */}
+        <div className="hotdog-layout">
+          <div className="layout-child">1</div>
+          <div className="layout-child">2</div>
+        </div>
+        {/* Hamburger layout */}
+        <div className="hamburger-layout">
+          <div className="layout-child">1</div>
+          <div className="layout-child">2</div>
+        </div>
+        {/* 2/3 1/3 vertical layout */}
+        <div className="twothird-onethird-layout">
+          <div className="layout-child">1</div>
+          <div className="layout-child">2</div>
+        </div>
+        {/* Vertiacl 1/2 vertical 1/2 divided */}
+        <div className="oneandtwo-layout">
+          <div className="layout-child">1</div>
+          <div className="oneandtwo-child">
+            <div className="layout-child">2</div>
+            <div className="layout-child">3</div>
+          </div>
+        </div>
+        {/* Quartered page */}
+        <div className="quarters-layout">
+          <div className="quarters-child">
+            <div className="layout-child">1</div>
+            <div className="layout-child">2</div>
+          </div>
+          <div className="quarters-child">
+            <div className="layout-child">3</div>
+            <div className="layout-child">4</div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
